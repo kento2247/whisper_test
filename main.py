5724086c1e2d5b1a3e23c1eca1b02003c2967d2f
kento2247 <tkento1985@icloud.com>
2024-04-15 11:46:27 +0900

    use dotenv

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-import whisper
 import json
+import os
 import sys
 
-def main():
-    model_path = "large"    #モデルのパス. small or medium or large
-    output_path = "output/transcription.txt"    #出力先のパス
+import whisper
+from dotenv import load_dotenv
 
+load_dotenv()  # 環境変数を読み込む
+
+
+def check_args():
     # コマンドライン引数の数が正しいか確認
     if len(sys.argv) < 2:
         print("Usage: python main.py <audio_file_path>")
         sys.exit(1)
-    audio_file_path = sys.argv[1]    # コマンドライン引数からファイルパスを取得
 
-    model = whisper.load_model(model_path) #モデルの読み込み
-    result = model.transcribe(audio_file_path, verbose=True, fp16=False, language="ja") #ファイル指定
-    print(result['text'])
 
-    f = open(output_path, 'w', encoding='UTF-8')
-    f.write(json.dumps(result['text'], sort_keys=True, indent=4, ensure_ascii=False))
-    f.close()
+def main():
+    model_path = os.getenv("MODEL_PATH")  # モデルのパス
+    input_path = sys.argv[1]  # コマンドライン引数からファイルパスを取得
+    output_path = os.getenv("OUTPUT_PATH")  # 出力先のパス
 
-if __name__ == '__main__':
+    output_path = output_path + input_path.split("/")[-1].replace(".m4a", ".json")
+
+    model = whisper.load_model(model_path)  # モデルの読み込み
+    result = model.transcribe(
+        input_path, verbose=True, fp16=False, language="ja"
+    )  # ファイル指定
+
+    with open(output_path, "w") as f:
+        json.dump(result, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    check_args()
     main()
